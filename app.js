b1655bcfb4201d2dd238267ee78d130768dbb82d
Nerota Karinos <ilhamar98@gmail.com>
2023-02-09 20:02:37 +0700

    changing discord modules to 14.7.1 and change from legacy to interaction

---
+++ app.js
@@ -0,0 +1,89 @@
+import fetch from "node-fetch";
+import * as dotenv from 'dotenv';
+import {Client, GatewayIntentBits, REST, Routes, userMention} from 'discord.js';
+
+dotenv.config();
+//const config = require("./config/config");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+//client.login(process.env.BOT_TOKEN);
+//OR
+//client.login(config.TOD_BOT.TOKEN);
+//client.disconnect();
+
+function getQuote() {
+    return fetch("https://zenquotes.io/api/random")
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            return data[0]["q"] + " -" + data[0]["a"]
+        })
+}
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+    {
+        name: 'tod',
+        description: 'Spin RAMBOT E someone',
+        options: [
+            {
+                name: "member",
+                description: "Who?",
+                type: 6,
+                required: true,
+
+            },
+        ]
+    },
+    {
+        name: 'inspire',
+        description: 'Get a quote',
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+});
+
+client.on('interactionCreate', async interaction => {
+    //console.log(`${msg.author.username}: ${msg.content.toString()}`);
+    const stringifiedObject = JSON.stringify(interaction.options, null, 2);
+    const objectifiedJson = JSON.parse(stringifiedObject)
+    console.log(`Message received: ${interaction.commandName}`);
+    console.log(stringifiedObject)
+    console.log(objectifiedJson._hoistedOptions[0].user.id)
+    if (!interaction.guild) return;
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'inspire') {
+        await getQuote().then(quote => interaction.reply(quote))
+    }
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+    if (interaction.commandName === 'tod') {
+        await interaction.reply(`z!spin RAMBOT E ${userMention(objectifiedJson._hoistedOptions[0].user.id)}`);
+    }
+});
+
+client.login(process.env.BOT_TOKEN).then(r =>
+console.log("Login Successful " + r))
